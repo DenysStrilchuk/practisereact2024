63ec97f267da440e82d10257a8305ca4f33d9d7b
Denys Sttrilchuk <denia876@gmail.com>
2024-02-08 16:43:35 +0200

    practise HW-2 part 4

--- src/components/UsersContainer/Users.js
+++ src/components/UsersContainer/Users.js
@@ -1,16 +1,15 @@
 import {useEffect, useState} from "react";
 import {usersService} from "../../services/usersService";
 import {User} from "../UserContainer/User";
-import {usersAPIService} from "../../services/usersAPIService";
 
 const Users = () => {
 const {users, setUsers} = useState([]);
     useEffect(() => {
-        usersAPIService.getAll().then(({data}) => setUsers(data));
+        usersService.getAll().then(({data}) => setUsers(data));
     }, []);
     return (
         <div>
-            {users.map(user => <User user={user}/>)}
+            {users.map(user => <User key={user.id} user={user}/>)}
         </div>
     );
 };
