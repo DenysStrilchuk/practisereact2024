2aba991813595e0591702be72571ce1e452f74bf
Denys Sttrilchuk <denia876@gmail.com>
2024-02-12 13:04:52 +0200

    practise HW-3 part 3

--- src/services/carsService.js
+++ src/services/carsService.js
@@ -3,8 +3,9 @@ import {urls} from "../constants/urls";
 
 const carsService = {
     getAll: () => apiService.get(urls.cars.base),
-    getById: (id) => apiService.get(urls.cars.byId),
+    getById: (id) => apiService.get(urls.cars.byId(id)),
     create: (data) => apiService.post(urls.cars.base, data),
-    delete: (data) => apiService.delete(urls.cars.base, data),
-    update: (data)
+    deleteById: (id) => apiService.delete(urls.cars.byId(id)),
+    updateById: (id, data) => apiService.post(urls.cars.byId(id), data)
 }
+
