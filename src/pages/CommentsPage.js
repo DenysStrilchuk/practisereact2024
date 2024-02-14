11de366c03a3ccea75fc4899c54219a8df911b0a
Denys Sttrilchuk <denia876@gmail.com>
2024-02-14 16:42:53 +0200

    lesson 4(practise) all done and style for comments

--- src/pages/CommentsPage.js
+++ src/pages/CommentsPage.js
@@ -1,9 +1,10 @@
 import {Comments} from "../components/CommentsContainer/Comments";
 import {Outlet} from "react-router-dom";
+import css from "../components/Main/MainCommentsStyle/CommentPage.module.css"
 
 const CommentsPage = () => {
     return (
-        <div>
+        <div className={css.Comment}>
             <Comments/>
             <Outlet/>
         </div>
