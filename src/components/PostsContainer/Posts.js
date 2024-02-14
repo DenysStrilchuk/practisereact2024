13453ac48a8e8dee88725773ec5e910fb7c46e61
Denys Sttrilchuk <denia876@gmail.com>
2024-02-14 15:59:11 +0200

    lesson 4(practise) doing for commentsPage outlet

--- src/components/PostsContainer/Posts.js
+++ src/components/PostsContainer/Posts.js
@@ -5,14 +5,14 @@ import {postsService} from "../../services/postsService";
 const Posts = () => {
     const{state:{postId}} = useLocation();
 
-    const [posts, setPosts] = useState(null);
+    const [post, setPosts] = useState(null);
 
     useEffect(() => {
         postsService.getById().then(({data}) => setPosts(data))
     }, []);
     return (
         <div>
-            {posts && <Post key={post.id}/>}
+            {post && <Post key={post.postId} post={post}/>}
         </div>
     );
 };
