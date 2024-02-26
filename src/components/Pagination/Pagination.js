c39a37219ca24c97359e761db358ccffe214a322
Denys Sttrilchuk <denia876@gmail.com>
2024-02-26 09:42:56 +0200

    make mini-project on js (realized  buttons onclick go to some page )

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,7 +2,6 @@ const Pagination = ({ currentPage, totalPages, setPage }) => {
     const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
     const startPage = Math.max(1, currentPage - 5);
     const endPage = Math.min(totalPages, currentPage + 5);
-    console.log(setPage);
 
     return (
         <div>
