f22780e31efac3bb88e25e18d6a04f2c6848ce35
Denys Sttrilchuk <denia876@gmail.com>
2024-02-29 22:54:41 +0200

    done stylisation on search page

--- src/components/MovieInfoContainer/MovieInfo.js
+++ src/components/MovieInfoContainer/MovieInfo.js
@@ -1,6 +1,8 @@
-import css from "./MovieInfo.module.css"
+import css from "./MovieInfo.module.css";
 
 const MovieInfo = ({overview, release, runtime, genres}) => {
+
+
     return (
         <div>
             <h2>Overview:</h2>
@@ -8,7 +10,7 @@ const MovieInfo = ({overview, release, runtime, genres}) => {
             <h2>Genres</h2>
             <div className={css.genres}>
                 {genres.map((genres, index) => (
-                    <p key={index} className={css.genre}>{genres.name}</p>
+                    <p key={index} className={css.genre}>{genres.name} </p>
                 ))}
             </div>
             <h2>Runtime</h2>
