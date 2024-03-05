087cf0e475dcc57299f88b6ad7e5a192ddbe60f3
Denys Sttrilchuk <denia876@gmail.com>
2024-03-05 20:56:20 +0200

    training HW7

---
+++ src/components/Users.js
@@ -0,0 +1,25 @@
+import {useDispatch, useSelector} from "react-redux";
+import {addUser} from "../store";
+
+const Users = () => {
+    const {users} = useSelector(state => state.user);
+    const dispatch = useDispatch();
+
+    const submit = (e) => {
+        e.preventDefault();
+        const name = e.target.userName.value;
+        const user = {id: new Date().getTime(), name};
+        dispatch(addUser({user}));
+    }
+    return (
+        <div>
+            <form onSubmit={submit}>
+                <input type="text" placeholder={'user'} name={'userName'}/>
+                <button>save</button>
+            </form>
+            {users.map(user => <div key={user.id}>{user.name}</div> )}
+        </div>
+    );
+};
+
+export {Users};
--- src/store/slices/user.slice.js
+++ src/store/slices/user.slice.js
@@ -9,8 +9,15 @@ let initialState = {
 };
 const userSlice = createSlice({
     name: 'userSlice',
-    initialState
+    initialState,
+    reducers: {
+        addUser: (state, action) => {
+            const user = action.payload.user;
+            state.users.push(user)
+        }
+    }
 });
 
 const userReducer = userSlice.reducer;
+export const {addUser} = userSlice.actions;
 export default userReducer;
