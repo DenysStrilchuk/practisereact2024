10fbc728b7cc85630dc752ee7a71b5324b009e37
Denys Sttrilchuk <denia876@gmail.com>
2024-03-10 09:46:20 +0200

    training HW8

--- src/components/CharactersContainer/Characters/Characters.js
+++ src/components/CharactersContainer/Characters/Characters.js
@@ -1,10 +1,27 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate, useParams} from "react-router-dom";
+import {useEffect} from "react";
+
+import {characterActions} from "../../../store/slices";
+import {Character} from "../Character";
+import css from "./Characters.module.css";
 
 const Characters = () => {
     const {characters} = useSelector(state => state.characters);
+    const {ids} = useParams();
+    const dispatch = useDispatch()
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        dispatch(characterActions.getByIds({ids}));
+    }, [ids, dispatch]);
+
     return (
         <div>
-            Characters
+            <button onClick={navigate(-1)}>back</button>
+            <div className={css.Characters}>
+                {characters.map(character => <Character key={character.id} character={character}/>)}
+            </div>
         </div>
     );
 };
